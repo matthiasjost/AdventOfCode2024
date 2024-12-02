e4bb320f88150914e6a4d8464e1df327f3929b63
Matthias Jost <matthias.jost@gmail.com>
2024-12-02 15:13:04 +0100

    test part 2

--- Day2/ReportAnalyzer.vb
+++ Day2/ReportAnalyzer.vb
@@ -16,63 +16,32 @@
     End Function
 
     Private Function CheckIfReportSafe(report As List(Of Integer)) As Boolean
-
-        If IsContantlyIncreasing(report) Or IsContantlyDecreasing(report) Then
+        If IsConstantlyIncreasing(report) OrElse IsConstantlyDecreasing(report) Then
             If IsDistanceInAllowedRange(report) Then
                 Return True
-            Else
-                Return False
+
             End If
-        Else
-            Return False
+
         End If
 
         Return False
 
     End Function
 
-    Private Function IsContantlyIncreasing(report As List(Of Integer)) As Boolean
-        Dim increase = True
-
-        For i = 0 To report.Count - 2
-            If report(i) < report(i + 1) Then
-                increase = True
-            Else
-                increase = False
-                Exit For
-            End If
-        Next
-
-        Return increase
+    Private Function IsConstantlyIncreasing(report As List(Of Integer)) As Boolean
+        Dim count = report.Zip(report.Skip(1), Function(a, b) a < b).Count(Function(x) x)
+        Return count >= (report.Count() - _tolerancePerLine - 1)
     End Function
 
-    Private Function IsContantlyDecreasing(report As List(Of Integer)) As Boolean
-        Dim decrease = True
 
-        For i = 0 To report.Count - 2
-            If report(i) > report(i + 1) Then
-                decrease = True
-            Else
-                decrease = False
-                Exit For
-            End If
-        Next
-
-        Return decrease
+    Private Function IsConstantlyDecreasing(report As List(Of Integer)) As Boolean
+        Dim count = report.Zip(report.Skip(1), Function(a, b) a > b).Count(Function(x) x)
+        Return count >= (report.Count() - _tolerancePerLine - 1)
     End Function
 
     Private Function IsDistanceInAllowedRange(report As List(Of Integer)) As Boolean
-        Dim distance = 0
-        Dim allowedRange = True
-        For i = 0 To report.Count - 2
-            distance = Math.Abs(report(i) - report(i + 1))
-
-            If distance > 3 Then
-                allowedRange = False
-            End If
-        Next
-
-        Return allowedRange
+        Dim count = report.Zip(report.Skip(1), Function(a, b) Math.Abs(a - b) < 4).Count(Function(x) x)
+        Return count >= (report.Count() - _tolerancePerLine - 1)
     End Function
 
 End Class
