e1919aa09af3b0ebb550e4693ce0f941a35fa1dd
Matthias Jost <matthias.jost@gmail.com>
2024-12-03 21:20:03 +0100

    refactor day 3

--- Day3/Multiplier.vb
+++ Day3/Multiplier.vb
@@ -6,13 +6,12 @@ Public Class Multiplier
     Private _remainingText As String
     Private _enabled As Boolean = True
 
-    Function Multiply(text As String) As Integer
-
+    Public Function Multiply(text As String) As Integer
         _remainingText = text
 
         While _remainingText.Length > 8
-            Dim result = ParseBrackets()
-            If _enabled Then
+            Dim result = ParseNextMatch()
+            If result.success AndAlso _enabled Then
                 Total += result.firstNumber * result.secondNumber
             End If
         End While
@@ -20,36 +19,28 @@ Public Class Multiplier
         Return Total
     End Function
 
-    Function ParseBrackets() As (firstNumber As Integer, secondNumber As Integer, success As Boolean)
-        Dim patterns As String() = {"mul\((\d+),(\d+)\)", "don't\(\)", "do\(\)"}
-        Dim matches As List(Of Match) = New List(Of Match)
-        For Each pattern In patterns
-            matches.Add(Regex.Match(_remainingText, pattern))
-        Next
+    Private Function ParseNextMatch() As (firstNumber As Integer, secondNumber As Integer, success As Boolean)
+        Dim pattern = "mul\((\d+),(\d+)\)|don't\(\)|do\(\)"
+        Dim match = Regex.Match(_remainingText, pattern)
 
-        Dim match = matches.OrderBy(Function(m) m.Index).FirstOrDefault(Function(m) m.Success)
-
-        If match Is Nothing Then
+        If Not match.Success Then
             Return (0, 0, False)
         End If
-
-        If match.Value = "don't()" Then
-            _enabled = False
-            _remainingText = _remainingText.Substring(match.Index + match.Length)
-            Return (0, 0, False)
-        End If
-
-        If match.Value = "do()" Then
-            _enabled = True
-            _remainingText = _remainingText.Substring(match.Index + match.Length)
-            Return (0, 0, False)
-        End If
-
-        Dim firstNumber = Integer.Parse(match.Groups(1).Value)
-        Dim secondNumber = Integer.Parse(match.Groups(2).Value)
 
         _remainingText = _remainingText.Substring(match.Index + match.Length)
-        Return (firstNumber, secondNumber, True)
+
+        Select Case match.Value
+            Case "don't()"
+                _enabled = False
+                Return (0, 0, False)
+            Case "do()"
+                _enabled = True
+                Return (0, 0, False)
+            Case Else
+                Dim firstNumber = Integer.Parse(match.Groups(1).Value)
+                Dim secondNumber = Integer.Parse(match.Groups(2).Value)
+                Return (firstNumber, secondNumber, True)
+        End Select
     End Function
 
 End Class
