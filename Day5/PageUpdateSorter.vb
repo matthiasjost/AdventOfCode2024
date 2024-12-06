444348bd8b35ad3d582db93fd32e64e9c6de2cab
Matthias Jost <matthias.jost@gmail.com>
2024-12-06 14:43:37 +0100

    Day 5 (WIP)

---
+++ Day5/PageUpdateSorter.vb
@@ -0,0 +1,37 @@
+﻿Public Class PageUpdateSorter
+    Public Function SortPages(orderingRules As String, pageUpdates As String) As Integer
+        Dim rules = parseRules(orderingRules)
+
+        Dim updates = parseUpdates(pageUpdates)
+
+        Return 0
+    End Function
+
+    Private Function parseUpdates(pageUpdatesString As String) As List(Of List(Of Integer))
+        Dim splitted = pageUpdatesString.Split(vbCrLf)
+
+        Dim pageUpdatesList As New List(Of List(Of Integer))
+
+        For Each r In splitted
+            Dim parts = r.Split(",")
+            Dim pages As New List(Of Integer)
+            For Each p In parts
+                pages.Add(CInt(p))
+            Next
+
+            pageUpdatesList.Add(pages)
+        Next
+        Return pageUpdatesList
+    End Function
+
+    Private Function parseRules(orderingRules As String) As List(Of Integer())
+        Dim splitted = orderingRules.Split(vbCrLf)
+        Dim rules As New List(Of Integer())
+        For Each r In splitted
+            Dim parts = r.Split("|")
+            rules.Add(New Integer() {CInt(parts(0)), CInt(parts(1))})
+        Next
+        Return rules
+    End Function
+
+End Class
---
+++ Tests/Day5Tests.vb
@@ -0,0 +1,52 @@
+﻿Imports Day5
+Imports Xunit
+
+Namespace Tests
+
+    Public Class Day5Tests
+        <Fact>
+        Sub Test1()
+            ' Arrange
+            Dim pageUpdateSorter = New PageUpdateSorter()
+
+            ' Act
+            Dim orderRules As String =
+        "47|53" & vbCrLf &
+        "97|13" & vbCrLf &
+        "97|61" & vbCrLf &
+        "97|47" & vbCrLf &
+        "75|29" & vbCrLf &
+        "61|13" & vbCrLf &
+        "75|53" & vbCrLf &
+        "29|13" & vbCrLf &
+        "97|29" & vbCrLf &
+        "53|29" & vbCrLf &
+        "61|53" & vbCrLf &
+        "97|53" & vbCrLf &
+        "61|29" & vbCrLf &
+        "47|13" & vbCrLf &
+        "75|47" & vbCrLf &
+        "97|75" & vbCrLf &
+        "47|61" & vbCrLf &
+        "75|61" & vbCrLf &
+        "47|29" & vbCrLf &
+        "75|13" & vbCrLf &
+        "53|13"
+
+            Dim pageUpdates As String = "75,47,61,53,29" & vbCrLf &
+        "97,61,53,29,13" & vbCrLf &
+        "75,29,13" & vbCrLf &
+        "75,97,47,61,53" & vbCrLf &
+        "61,13,29" & vbCrLf &
+        "97,13,75,29,47"
+
+            Dim sorter = New PageUpdateSorter()
+            Dim middlePageTotal = sorter.SortPages(orderRules, pageUpdates)
+
+            ' Assert
+            Assert.Equal(75, middlePageTotal)
+        End Sub
+
+    End Class
+
+End Namespace
